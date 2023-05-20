00747c61c6b4dbc6c46bb223a9e9cece993817bb
Gaurav Kumar <geekay@Gauravs-MacBook-Air.local>
2023-05-20 20:06:01 +0530

    adding initial project files

---
+++ run_kubernetes.sh
@@ -0,0 +1,22 @@
+.#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=gauravkr04/udacity-housing
+echo $dockerpath
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run udacity-housing --image=$dockerpath:v1.0.0 --port=80
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward udacity-housing 8000:80
